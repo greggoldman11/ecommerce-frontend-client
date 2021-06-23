8b01037def25ad2fa483cfaf0e9eb4aec0f454bd
ktresel2 <ktreseler2@gmail.com>
2021-06-23 15:17:24 -0400

    Add Index route

--- src/components/Product/IndexProducts.js
+++ src/components/Product/IndexProducts.js
@@ -1,1 +1,27 @@
+import React, { Component, Fragment } from 'react'
 
+import { indexProducts } from './../../api/products'
+
+class IndexProducts extends Component {
+  constructor () {
+    super()
+    this.state = {
+      products: null
+    }
+  }
+
+  componentDidMount () {
+    indexProducts()
+      .then(res => this.setState({ products: res.data.products }))
+  }
+
+  render () {
+    return (
+      <Fragment>
+        <h2>Index movies page</h2>
+      </Fragment>
+    )
+  }
+}
+
+export default IndexProducts

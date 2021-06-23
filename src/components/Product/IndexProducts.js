32fcf62def003eb069fd85c1d121e5b7ad3a729b
ktresel2 <ktreseler2@gmail.com>
2021-06-23 17:13:49 -0400

    Nothing new

--- src/components/Product/IndexProducts.js
+++ src/components/Product/IndexProducts.js
@@ -13,10 +13,8 @@ class IndexProducts extends Component {
   }
 
   componentDidMount () {
-    console.log(this.state)
     indexProducts()
       .then(res => this.setState({ products: res.data.products }))
-    console.log(this.state)
   }
 
   render () {

51756734273af7057b9cb4fb970631ce5423a520
vikkimccrory <victoriajmccrory@gmail.com>
2021-06-28 19:32:27 -0400

    Cart update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,21 +1,17 @@
 import React, { Component, Fragment } from 'react'
 import { withRouter } from 'react-router-dom'
 import Spinner from 'react-bootstrap/Spinner'
-
 import { getAllCarts, removeFromCart } from './../../api/cart'
-import Stripe from '../Checkout/Stripe.js'
 
 class Cart extends Component {
-  constructor (props) {
-    super(props)
-
+  constructor () {
+    super()
     this.state = {
       products: null,
       cartId: null,
       completed: false
     }
   }
-
   componentDidMount () {
     getAllCarts(this.props.user)
       .then(allCarts => {
@@ -23,15 +19,14 @@ class Cart extends Component {
         return (allCarts.data.carts.filter(cart => cart.completed === false))
       })
       .then(res => {
-        console.log(res[0].products[1].price + res[0].products[0].price)
+        console.log(res)
         this.setState({ products: res[0].products, cartId: res[0]._id })
       })
   }
   render () {
     const { cartId, products } = this.state
-    // console.log(cartId, products)
+    console.log(cartId, products)
     let cartJsx = ''
-
     if (this.state.products === null) {
       cartJsx = <Spinner animation="border" variant="warning" />
     } else if (this.state.products.length === 0) {
@@ -43,28 +38,30 @@ class Cart extends Component {
               <div key={product._id}>
                 <h3>{product.name}</h3>
                 <p>{product.price}</p>
-                <p>Total: {this.total}</p>
                 <button
                   onClick={() => {
                     removeFromCart(cartId, products[0]._id, this.props.user)
-                      .then(console.log('success'))
-                      .then(() => this.props.history.push('/products'))
-                      .catch(console.error)
+                      .then(getAllCarts(this.props.user))
+                      .then(allCarts => {
+                        console.log(allCarts)
+                        return (allCarts.data.carts.filter(cart => cart.completed === false))
+                      })
+                      .then(res => {
+                        console.log(res)
+                        this.setState({ products: res[0].products, cartId: res[0]._id })
+                      })
                   }}>
                 Remove From Cart</button>
               </div>
             )
           })
     }
-
     return (
       <Fragment>
         <h2>Cart Page</h2>
         {cartJsx}
-        <Stripe />
       </Fragment>
     )
   }
 }
-
 export default withRouter(Cart)

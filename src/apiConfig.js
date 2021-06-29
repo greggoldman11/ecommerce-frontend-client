ae9092560cddd2c13f870f6174327f3b4480e359
ktresel2 <ktreseler2@gmail.com>
2021-06-29 08:38:33 -0400

    Change apiUrl

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://blooming-lowlands-35038.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 

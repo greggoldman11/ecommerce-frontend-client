fab90bfde20538ad228066017472dbb528edeb6b
ktresel2 <ktreseler2@gmail.com>
2021-07-11 15:31:45 -0400

    Fix index error

--- src/components/Product/HomeIndexProducts.js
+++ src/components/Product/HomeIndexProducts.js
@@ -28,7 +28,7 @@ class HomeIndexProducts extends Component {
     } else {
       productsJSX =
           this.state.products.map((product, index) => (
-            return index < 4 &&
+            index < 4 &&
             <div key={product._id} className='d-flex row'>
               <ProductCard className="col-4"
                 id={product._id}
